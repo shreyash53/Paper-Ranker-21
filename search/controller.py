3e575453f34b9866cbc3dbb38f6e542ec8284113
Jayaraj Balagopal <jayarajbalagopal@Jayarajs-MacBook-Air.local>
2021-11-13 01:43:55 +0530

    Fixed API
    Signed-off-by: Jayaraj Balagopal <jayarajbalagopal@Jayarajs-MacBook-Air.local>

--- search/controller.py
+++ search/controller.py
@@ -30,7 +30,7 @@ def paper_add_helper(request_data):
         paper = None
     return paper
 
-def map_ranks(papers):
+def map_ranks(paper_dict):
     result = dict()
     result["papers"] = []
     count = 0
@@ -39,17 +39,16 @@ def map_ranks(papers):
     for paper in paper_dict["data"]:
         conference_ = getConference(paper["venue"])
         if conference_:
-            result["papers"].append(conference_)
-            result["conference"] = conference_  # storing the fetched conference obj
+            paper["conference"] = conference_
+            result["papers"].append(paper) 
             count = count + 1
         else:
             rank = get_conference_rank_from_web(paper["venue"])
             if rank != "Not found":
-                paper["rank"] = rank
+                conference_ = conference_add(rank["name"],rank["rank"],paper["venue"])
+                paper["conference"] = conference_
                 result["papers"].append(paper)
                 count = count + 1
-            else:
-                pass
     result["total"] = count
     return result
 
@@ -60,33 +59,29 @@ def paper_add(request_data):
     return paper.getObject(), HTTP_STATUS_OK
 
 def paper_search_helper(params, paper_result_set):
-    temp = PaperCollector.fetch_papers(params)
+    collector = PaperCollector()
+    temp = collector.fetch_papers(params["query"])
     fetched_papers = map_ranks(temp)
     for paper_ in fetched_papers["papers"]:
-        # paper_obj = dict(paper_structure)
+        print(type(paper_["conference"]))
+        paper_obj = dict(paper_structure)
         paper_obj = Paper(
             title=paper_["title"],
-            desciption=paper_["abstract"],
+            description=paper_["abstract"],
             year=paper_["year"],
             url=paper_["url"],
             author=",".join([author["name"] for author in paper_["authors"]]),
-            conference=paper_["conference"],
+            conference=paper_["conference"]
         )
         paper_result_set.insert(paper_obj)
-
-    # return paper_result_set
 
 def paper_search(params):
     paper_result_set = Paper.objects.filter(
         Q(title__icontains=params["query"]) | Q(description__icontains=params["query"])
     )
     if paper_result_set.count() < 5:
-        # do global search
         paper_search_helper(params, paper_result_set)
-
     return jsonify([paper_.getObject() for paper_ in paper_result_set]), HTTP_STATUS_OK
-    # return paper_result_set.to_json(), HTTP_STATUS_OK
-
 
 def getConference(conference_name):
     conference_rs = Conference.objects.filter(
@@ -96,10 +91,8 @@ def getConference(conference_name):
         return conference_rs.first()
     return None
 
-
 def conference_add(conference_name, conference_rank, conference_abbr):
     conference_ = Conference(
         name=conference_name, abbr=conference_abbr, rank=conference_rank
     )
-    conference_.save()
-    return conference_
+    return conference_.save()
--- search/routes.py
+++ search/routes.py
@@ -1,14 +1,13 @@
 from flask import Blueprint, render_template, request
-
 from search.controller import paper_add, paper_search
 from flask_cors import cross_origin
+from globe_search.apiengine import PaperCollector
 
 blueprint = Blueprint("Search", __name__, url_prefix="/paper")
 
 @blueprint.route("/")
 def home():
-	return render_template("index.html")
-    #return render_template("search.html")
+    return render_template("search.html")
 
 @blueprint.route("/search", methods=["GET"])
 @cross_origin()
